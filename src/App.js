d7de1a4ed6334b76f2381537fec726bd5af5b2ff
nbappi <bappi.cse562@gmail.com>
2021-07-08 15:10:00 +0600

    Write hello something with functions components or stateless

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-function App() {
-  return (
-    <div className="App">
-      Hello world
-    </div>
-  );
+function Hello(props){
+  const tag= "H2";
+    return (
+      <p>Writer Hello { props.name} with {tag} tag</p>
+    )
+}
+
+function App(){
+
+  const elem1 = <Hello name="Nuruzzaman" />
+
+    return (
+      <div>
+          {elem1}
+          <Hello name="Bappi" />
+      </div>
+    )
+
 }
 
 export default App;
